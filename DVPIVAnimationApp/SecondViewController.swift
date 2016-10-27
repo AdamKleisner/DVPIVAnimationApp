000b9d800678dd7a2aca126dc94d109441e61086
Adam <awkleisnerfullsail.edu>
2016-10-27 17:04:54 -0400

    Initial Commit

---
+++ DVPIVAnimationApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DVPIVAnimationApp
+//
+//  Created by Adam Kleisner on 10/27/16.
+//  Copyright © 2016 Adam Kleisner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
